98456a221ebf6f4a6215fe8d8794c39f097d4429
ValeriyaRyzhikova <rvaleriav@yandex.ru>
2021-08-16 19:18:40 +0700

    Defined VirtualTest class

--- VirtualTest.cpp
+++ VirtualTest.cpp
@@ -1,1 +1,29 @@
 #include "VirtualTest.h"
+
+VirtualTest::VirtualTest()
+{
+	name = new char[12];
+	name = "virtual test";
+	tc_id = 0;
+}
+
+VirtualTest::~VirtualTest()
+{
+	delete[] name;
+}
+
+int VirtualTest::Execute()
+{
+	try
+	{
+		Prep();
+		Run();
+	}
+	catch (const char* massange)
+	{
+		std::cout << "Test " << name << " ended with error: " << massange;
+		return -1;
+	}
+
+	ClrarUp();
+}
--- VirtualTest.h
+++ VirtualTest.h
@@ -1,5 +1,17 @@
 #pragma once
+#include<iostream>
+
 class VirtualTest
 {
+private:
+	int tc_id;
+	const char* name;
+	virtual void Prep() = 0;
+	virtual void Run() = 0;
+	virtual void ClrarUp() = 0;
+public:
+	VirtualTest();
+	virtual ~VirtualTest();
+	int Execute();
 };
 
