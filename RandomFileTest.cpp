c67c3163167064b4edbc1572ec1e86878d364efc
ValeriyaRyzhikova <rvaleriav@yandex.ru>
2021-08-18 16:20:51 +0700

    Fixed bags

--- RandomFileTest.cpp
+++ RandomFileTest.cpp
@@ -2,9 +2,8 @@
 
 RandomFileTest::RandomFileTest()
 {
-	name = new char[16];
-	name = "random file test";
-	tc_id = 2;
+	name = "random file";
+	tcId = RANDOM;
 }
 
 void RandomFileTest::Prep()
@@ -13,16 +12,21 @@ void RandomFileTest::Prep()
 	statex.dwLength = sizeof(statex);
 	GlobalMemoryStatusEx(&statex);
 
-	int memory_size = statex.ullTotalPhys % (1024*1024*1024);
+	int memory_size = statex.ullTotalPhys / (1024*1024*1024);
 	if (!memory_size)
 		throw "RAM less them a gigabyte";
 }
 
 void RandomFileTest::Run()
 {
-	std::ofstream file("test.txt");
-	std::mt19937_64 gen;
+	std::ofstream file("test.txt", std::ios::binary);
+	std::mt19937 gen;
 	for (int i = 0; i != 1024 * 1024; i++)
 		file << gen() % 2;
 	file.close();
+}
+
+void RandomFileTest::ClrarUp()
+{
+	remove("test.txt");
 }
